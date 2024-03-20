7d91ceb6855dbc41f725d3d1b3afbafbc256a4f4
bytelisa <elisa.marzioli2002@gmail.com>
2024-03-20 11:01:25 +0100

    Inserito nuovo pulsante di prova

--- src/main/java/com/example/studypal/HelloController.java
+++ src/main/java/com/example/studypal/HelloController.java
@@ -3,12 +3,21 @@ package com.example.studypal;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+//import java.text.BreakIterator;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label testoBenvenuto;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Hello from Elisa and Valentina :)");
     }
+
+
+
+    @FXML
+    protected void onCiaoButtonClick() { testoBenvenuto.setText("Ciao da Elisa e Valentina :)"); }
 }
