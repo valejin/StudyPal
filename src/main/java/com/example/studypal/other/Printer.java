93470941e545c5d54b6e1512f1ff25435228f7b3
valejin <valentina.jin@students.uniroma2.eu>
2024-03-27 18:10:42 +0100

    creato file properties per la DBconnessione

---
+++ src/main/java/com/example/studypal/other/Printer.java
@@ -0,0 +1,11 @@
+package com.example.studypal.other;
+
+import java.sql.SQLOutput;
+
+public class Printer {
+
+    //stampa messaggio di errore
+    public static void errorPrint(String message) {
+        System.out.print(message);
+    }
+}
