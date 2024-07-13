a6cf2c7dffb09d55298e61e29b38ac6a1e401b6e
bytelisa <elisa.marzioli2002@gmail.com>
2024-07-13 17:36:31 +0200

    prova query

--- src/main/java/com/example/studypal/query/Query.java
+++ src/main/java/com/example/studypal/query/Query.java
@@ -5,7 +5,7 @@ public class Query {
     public Query(){}
 
 
-/*--------------------Queries Login-----------------------------*/
+    /*--------------------Queries Login-----------------------------*/
 
     //Query per verificare se l'email inserita è già stata registrata (nel momento di LOGIN)
     public static final String RICERCA_EMAIL = "SELECT * FROM utente WHERE email = '%s' ";
@@ -15,7 +15,7 @@ public class Query {
 
 
 
-/*--------------------Queries Registrazione-------------------*/
+    /*--------------------Queries Registrazione-------------------*/
     public static final String REGISTRAZIONE = "INSERT INTO utente (email, nome, cognome, password, isTutor) VALUES ('%s', '%s', '%s', '%s', %b)";
 
     public static final String REGISTRAZ_TUTOR = "INSERT INTO tutor (email, tariffa, luogo, materie, inPresenza, webCam, giorni, nome, cognome) VALUES ('%s', NULL, NULL, NULL, NULL, NULL, NULL, '%s', '%s')";
