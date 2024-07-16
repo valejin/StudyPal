d5631b423333af509c963145301b853b68785032
bytelisa <elisa.marzioli2002@gmail.com>
2024-07-16 14:24:37 +0200

    test 3 - controllo prenotazione

--- src/test/java/TestStudente.java
+++ src/test/java/TestStudente.java
@@ -1,20 +1,27 @@
 import com.example.studypal.bean.LoggedInUserBean;
+import com.example.studypal.dao.PrenotazioneDAO;
 import com.example.studypal.dao.UserDAO;
 import com.example.studypal.exceptions.CredenzialiSbagliateException;
+import com.example.studypal.exceptions.NonProduceRisultatoException;
 import com.example.studypal.exceptions.PersistenzaNonValida;
 import com.example.studypal.exceptions.UtenteInesistenteException;
 import com.example.studypal.model.CredenzialiModel;
+import com.example.studypal.model.PrenotazioneModel;
 import com.example.studypal.other.FactoryDAO;
 import com.example.studypal.other.Printer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 class TestStudente {
 
      /** Elisa Marzioli 0310065 **/
 
     private static final String USER_EMAIL = "test@email.com";
-
+    private static final String TEST = "test";
     private static final LoggedInUserBean tutorTEST = new LoggedInUserBean("test@tutor.com", "test", "test");
     private static final LoggedInUserBean studenteTEST = new LoggedInUserBean("test@studente.com", "test", "test");
 
@@ -70,5 +77,25 @@ class TestStudente {
 
     public String generaEmail(){
         return "test" + System.currentTimeMillis() + "@email.com";
+    }
+
+    @Test
+    void controlloRichiesteInviate(){
+        /* faccio una prenotazione di test e controllo che venga registrata*/
+        List<Integer> valori = Arrays.asList(0,0,0);
+        PrenotazioneModel richiesta = new PrenotazioneModel(0, tutorTEST, USER_EMAIL,TEST, TEST,null,valori);
+
+        try{
+            PrenotazioneDAO dao = new PrenotazioneDAO();
+            dao.prenota(richiesta);
+            List<PrenotazioneModel> richieste = dao.richiesteArrivate(tutorTEST.getEmail(), 0);
+
+            //cancello per testing futuri
+            dao.cancellaRichiesta(richieste.getFirst().getIdRichiesta());
+
+            Assertions.assertEquals(1, richieste.size());
+        } catch (SQLException | NonProduceRisultatoException e) {
+            Assertions.fail();
+        }
     }
 }
